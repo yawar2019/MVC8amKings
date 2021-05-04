d7a8612b01ae5409372f3ed2925e72aa91455f35
yawar2019 <yawarali17@gmail.com>
2021-05-04 08:14:38 +0530

    committed

--- MVC8amKings/App_Start/RouteConfig.cs
+++ MVC8amKings/App_Start/RouteConfig.cs
@@ -13,11 +13,21 @@ namespace MVC8amKings
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+             name: "Default",
+             url: "{controller}/{action}/{id}",
+             defaults: new { controller = "new", action = "helloworld", id = UrlParameter.Optional }
+         );
+
+            routes.MapRoute(
+                name: "Default1",
+                url: "pistahouse/cake",
+                defaults: new { controller = "new", action = "helloworld", id = UrlParameter.Optional }
             );
+
+           
         }
     }
 }
---
+++ MVC8amKings/Controllers/NewController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC8amKings.Controllers
+{
+    public class NewController : Controller
+    {
+        // GET: New
+        [Route("Superman/shopping")]
+        [Route("batman/cricket")]
+        public string  helloworld() {
+
+            return "Hello to All";
+        }
+
+        public int helloworld2()
+        {
+
+            return 1211;
+        }
+
+        public ActionResult getView()
+        {
+
+            return View("Contact");
+        }
+        public ActionResult getViewofDefault()
+        {
+
+            return View("~/Views/Default/index.cshtml");
+        }
+        public string helloworld12(string id)
+        {
+
+            return "Hello to All "+ id;
+        }
+
+    }
+}
