b0a7962dae7b363645a0343fe5394161baf11c57
yawar2019 <yawarali17@gmail.com>
2021-05-06 08:59:33 +0530

    commit

--- MVC8amKings/Controllers/NewController.cs
+++ MVC8amKings/Controllers/NewController.cs
@@ -112,5 +112,104 @@ namespace MVC8amKings.Controllers
 
             return View();
         }
+
+        public ActionResult sendName4()
+        {
+            EmployeeModel obj = new EmployeeModel();
+            obj.EmpId = 1;
+            obj.EmpName = "Trupti";
+            obj.EmpSalary = 578000;
+
+            // object model = obj;
+
+          
+            return View(obj);
+        }
+
+
+        public ActionResult sendName5()
+        {
+            EmployeeModel obj = new EmployeeModel();
+            obj.EmpId = 1;
+            obj.EmpName = "Trupti";
+            obj.EmpSalary = 578000;
+
+
+            EmployeeModel obj1 = new EmployeeModel();
+            obj1.EmpId = 2;
+            obj1.EmpName = "Dash";
+            obj1.EmpSalary = 538000;
+
+
+            EmployeeModel obj2 = new EmployeeModel();
+            obj2.EmpId = 3;
+            obj2.EmpName = "Namaithullah";
+            obj2.EmpSalary = 378000;
+
+
+            EmployeeModel obj3 = new EmployeeModel();
+            obj3.EmpId = 4;
+            obj3.EmpName = "ElayRaja";
+            obj3.EmpSalary = 478000;
+
+            List<EmployeeModel> listEmp = new List<EmployeeModel>();
+            listEmp.Add(obj);
+            listEmp.Add(obj1);
+            listEmp.Add(obj2);
+            listEmp.Add(obj3);
+
+            return View(listEmp);
+        }
+
+        public ActionResult sendName6()
+        {
+            EmployeeModel obj = new EmployeeModel();
+            obj.EmpId = 1;
+            obj.EmpName = "Trupti";
+            obj.EmpSalary = 578000;
+
+
+            EmployeeModel obj1 = new EmployeeModel();
+            obj1.EmpId = 2;
+            obj1.EmpName = "Dash";
+            obj1.EmpSalary = 538000;
+
+
+            EmployeeModel obj2 = new EmployeeModel();
+            obj2.EmpId = 3;
+            obj2.EmpName = "Namaithullah";
+            obj2.EmpSalary = 378000;
+
+
+            EmployeeModel obj3 = new EmployeeModel();
+            obj3.EmpId = 4;
+            obj3.EmpName = "ElayRaja";
+            obj3.EmpSalary = 478000;
+
+            List<EmployeeModel> listEmp = new List<EmployeeModel>();
+            listEmp.Add(obj);
+            listEmp.Add(obj1);
+            listEmp.Add(obj2);
+            listEmp.Add(obj3);
+
+
+            DepartmentModel dept1 = new DepartmentModel();
+            dept1.DeptId = 1211;
+            dept1.DeptName = "IT";
+
+            DepartmentModel dept2 = new DepartmentModel();
+            dept2.DeptId = 1211;
+            dept2.DeptName = "Network";
+
+            List<DepartmentModel> listdept = new List<DepartmentModel>();
+
+            listdept.Add(dept1);
+            listdept.Add(dept2);
+
+
+
+
+            return View(listEmp);
+        }
     }
 }
